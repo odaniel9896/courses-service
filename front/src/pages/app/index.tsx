d829c021c4be229fb2edd6ce38c9a925eb80902f
Daniel <soumenorgame@gmail.com>
2023-01-02 20:22:45 -0300

    feat: finalize configure apollo and install tailwind

--- front/src/pages/app/index.tsx
+++ front/src/pages/app/index.tsx
@@ -1,24 +1,44 @@
-import { getAccessToken, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import Link from "next/link";
+import { withApollo } from "../../lib/withApollo";
+import { useMeQuery } from "../../graphql/generated/graphql";
+import {
+  ssrGetProducts,
+  getServerPageGetProducts,
+} from "../../graphql/generated/page";
 
-export default function Home() {
+function Home({ data }: any) {
+  console.log("🚀 ~ file: index.tsx:11 ~ Home ~ data", data);
   const { user } = useUser();
+  const { data: me, loading, error } = useMeQuery();
+
+  console.log(me);
+  // if (error) {
+  //   return <h1>error</h1>;
+  // }
+
+  // if (loading) {
+  //   return <h1>loading</h1>;
+  // }
+
+  // console.log(data);
 
   return (
     <div>
       <h1>hell world</h1>
+      <pre>{JSON.stringify(data.products, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
-      <Link href="/api/auth/logout" />
     </div>
   );
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ req, res }) => {
-    console.log(await getAccessToken(req, res));
+  getServerSideProps: async (ctx) => {
     return {
       props: {},
     };
+    // return getServerPageGetProducts({}, ctx);
   },
 });
+
+export default withApollo(ssrGetProducts.withPage()(Home));
